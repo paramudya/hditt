70941c9178fc622fa319e77b0feb36c39c003d2b
unknown <BNI@DMA-61668-1.hq.bni.co.id>
2022-08-28 17:58:03 +0700

    Add existing file

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ app = Flask (__name__)
 @app.route('/')
 def hello():
     return render_template("home.html")
+
 @app.route("/result", methods=['POST','GET'])
 def namafungsiirelevankah():
     data_input = (request.form)
-    input_string=data_input.get('text')
-
+    input_string=data_input.get('string_name') 
     output_string=hard_code(input_string)
 
-    return render_template("result.html",input=data_input,result=output_string)
+    return render_template("result.html",input=input_string,result=output_string)
 
 if __name__ == '__main__':
     app.run()
