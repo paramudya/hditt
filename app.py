c8ac04817810904084742a5d1b476dc023cd2000
unknown <BNI@DMA-61668-1.hq.bni.co.id>
2022-08-28 16:07:10 +0700

    Add existing file

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask (__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return 'Hello this is the dummy for us dummies to deply our dumb dumb depressing project!'
 
 if __name__ == '__main__':
     app.run()
