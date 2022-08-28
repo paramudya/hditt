928013530010aaba43fbb75c78a98c0439d940e7
unknown <BNI@DMA-61668-1.hq.bni.co.id>
2022-08-28 17:21:31 +0700

    Add existing file

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template, request, jsonify
+from secret import hard_code
 
 app = Flask (__name__)
 
 @app.route('/')
 def hello():
     return render_template("home.html")
+@app.route("/result", methods=['POST','GET'])
+def namafungsiirelevankah():
+    data_input = (request.form)
+    # input_string=data_input.get('text')
+
+    output_string=hard_code(input_string)
+
+    return render_template("result.html",input='asd',result=output_string)
 
 if __name__ == '__main__':
     app.run()
---
+++ calc.py
@@ -0,0 +1,19 @@
+lst_of_depressing_w=['sad', 'sick of', 'fuck it','fuck that','fuck it',':(']
+count=0
+input_sent='So sad of this, fuck it'
+
+def hard_code(stringg):
+    for word in lst_of_depressing_w:
+        if word.lower() in stringg.lower():
+            count+=1
+    return round(count/len(input_sent.split())*100,2)
+
+# if __name__ == '__main__':
+#     n = int(input("number of test cases: "))
+#     ints = list(map(int,input("insert {} integer{} each separated with space:\n".format(n,'' if n <= 1 else 's')).split()))
+
+#     try:
+#         for i in range(n):
+#             print(nine(ints[i]))
+#     except:
+#         print("number of integers you just put in is less than what you said earlier!")
